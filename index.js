4a793550efa32b40b47aca8ffd49b5d9e8dfcc43
Travis Russell <mtrussell@gmail.com>
2021-08-17 20:28:16 -0600

    added prompts for manager information

--- index.js
+++ index.js
@@ -1,4 +1,58 @@
+// package includes
+const inquirer = require('inquirer');
+// Class includes
 const Employee = require('./lib/Employee.js');
 const Engineer = require('./lib/Engineer.js');
 const Intern = require('./lib/Intern.js');
 const Manager = require('./lib/Manager.js');
+
+inquirer.prompt([
+    {
+        type: 'input',
+        name: 'name',
+        message: 'Enter the team manager\'s name:',
+        validate: data => {
+            if (data) { return true; }
+            else {
+                console.log('Name cannot be blank.');
+                return false;
+            }
+        }
+    },
+    {
+        type: 'input',
+        name: 'id',
+        message: 'Enter the team manager\'s ID number:',
+        validate: data => {
+            if (data) { return true; }
+            else {
+                console.log('ID cannot be blank.');
+                return false;
+            }
+        }
+    },
+    {
+        type: 'input',
+        name: 'email',
+        message: 'Enter the team manager\'s email address:',
+        validate: data => {
+            if (data) { return true; }
+            else {
+                console.log('Email address cannot be blank.');
+                return false;
+            }
+        }
+    },
+    {
+        type: 'input',
+        name: 'office',
+        message: 'Enter the team manager\'s office number:',
+        validate: data => {
+            if (data) { return true; }
+            else {
+                console.log('Office number cannot be blank.');
+                return false;
+            }
+        }
+    }
+])
